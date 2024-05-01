b5f3334237b4fa89954ce61e1560519484067447
Ali-raza-01 <sh1134904@gmail.com>
2024-05-02 01:04:58 +0500

    prompt assignments

---
+++ javaScript-assignments/prompt assignments/app.js
@@ -0,0 +1,41 @@
+// // even/odd checker
+// var num = prompt("Enter a number");
+
+// if(num % 2 === 0 ){
+//     document.write("number is even")
+// } else {
+//     document.write("number is odd")
+// };
+
+// // positve negetive checker
+// var numChek = prompt("enter a number");
+
+// if(numChek >= 0){
+//     document.write("number is positive")
+    
+// } else{
+//     document.write("number is nagetive")
+// };
+
+// display larger number
+
+// var largeNum = prompt(' enter a number')
+// var largeNum1 = prompt('enter a number')
+
+// if(largeNum > largeNum1 && largeNum1 < largeNum  ){
+//     document.write(maxNum)
+// } else{
+//     document.write(maxNum1)
+// };
+
+// display the smallest number
+
+var smallNum = prompt(' enter a number');
+var smallNum1 = prompt('enter a number');
+var smallNum2 = prompt('enter a number');
+
+if(smallNum < smallNum1 && smallNum1 < smallNum && smallNum < smallNum2 ){
+    document.write(smallNum)
+} else{
+    document.write(smallNum2)
+};
